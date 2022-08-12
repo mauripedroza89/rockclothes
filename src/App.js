7048c18b040ea30745e23d9460cf8a34153e8eae
Mauricio Munoz <mauripedroza89@gmail.com>
2022-08-12 13:17:32 -0500

    fixing routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import  Cart  from './components/Cart/Cart';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";import  Cart  from './components/Cart/Cart';
 import Footer from './components/Footer/Footer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
@@ -14,10 +13,10 @@ function App() {
       <NavBar/>
       
       <Routes>
-        <Route path='/' element={<ItemListContainer/>} />
-        <Route path='/category/:categoryId' element={<ItemListContainer/>} />
-        <Route path='/detail/:productId' element={<ItemDetailContainer  />} />
-        <Route path='/cart' element={<Cart/>} />
+        <Route path='/' exact element={<ItemListContainer/>} />
+        <Route path='/category/:categoryId' exact element={<ItemListContainer/>} />
+        <Route path='/detail/:productId' exact element={<ItemDetailContainer  />} />
+        <Route path='/cart' exact element={<Cart/>} />
         {/* <Route path='/checkout' element={<Checkout/>} /> */}
       </Routes> 
       <Footer/>
