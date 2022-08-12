ec6e0fa77dc829452af40e3ce8b9bf18519dd4aa
Mauricio Munoz <mauripedroza89@gmail.com>
2022-08-12 13:23:02 -0500

    fixing routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";import  Cart  from './components/Cart/Cart';
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";import  Cart  from './components/Cart/Cart';
 import Footer from './components/Footer/Footer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
